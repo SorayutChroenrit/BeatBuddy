873cf6e4c1c8166e0e7f1b944f1b4d8ce90df832
Sorayut CH <sorayutapple@gmail.com>
2025-05-17 18:15:22 +0700

    fixed url

--- src/lib/auth.tsx
+++ src/lib/auth.tsx
@@ -1,7 +1,7 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = process.env.BACKEND_URL;
 
 const api = axios.create({
   baseURL: API_URL,
