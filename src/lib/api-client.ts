4440bde2915e1e3d090ca1f4234fc2f61306906b
Sorayut CH <sorayutapple@gmail.com>
2025-05-17 18:17:56 +0700

    fixed url

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.BACKEND_URL;
+const API_BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const apiClient = axios.create({
   baseURL: API_BASE_URL,
--- src/lib/auth.tsx
+++ src/lib/auth.tsx
@@ -1,7 +1,7 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
 import axios from "axios";
 
-const API_URL = process.env.BACKEND_URL;
+const API_URL = process.env.VITE_BACKEND_URL;
 
 const api = axios.create({
   baseURL: API_URL,
